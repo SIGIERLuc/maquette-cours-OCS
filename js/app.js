8536bb1b07e97ce5ad757f42cc389633ef134287
SIGIERLuc <lucsigier@gmail.com>
2019-03-28 11:33:01 +0100

    Insert Smooth scroll
    Insertion de app.js qui permet de faire des smooth scroll entre les anchors du site et ajout de la classe fixed-top a la navbar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
-(function($){
-
-    $('.owl-carousel').owlCarousel({
-        items: 3,
-        loop:true,
-        center:true,
+$(function () {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function () {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+            if (anchor.length) {
+                $("html, body").animate({ scrollTop: anchor.offset().top - 100 }, 1500);
+            }
+        }
     });
+});
+$(function () {
+    /**
+    * Smooth scrolling to the top of page
+    **/
+    $("#description").click(function () { 
+        $("html, body").animate({ scrollTop: 0 }, 1500); 
+    })
 
-})(jQuery);
+});
+$('.navbar-nav>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
 
