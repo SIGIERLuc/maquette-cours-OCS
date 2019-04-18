61948ec0580d0655aedd1f0d9925fb11493235bd
SIGIERLuc <lucsigier@gmail.com>
2019-04-18 20:23:44 +0200

    Revert "Finalisation of MVC"
    This reverts commit f6df3f36ed79409e82fea208fb620c58340db709.

--- php/template-project.php
+++ php/template-project.php
@@ -1,4 +1,4 @@
-<div class="collapse col-sm-12 projectFrame" id=<?= $project_results["ID"] ?> data-parent="#myGroup">
+<div class="collapse show col-sm-12 projectFrame" id=<?= $project_results["ID"] ?> data-parent="#myGroup">
     <h2>
         <?= $project_results["project_name"] ?>
     </h2>
@@ -6,10 +6,12 @@
         <?= $project_results["project_description"] ?>
     </p>
     <br>
+
+
     <?php
 
     foreach ($button_array as $key => $button) {
-        echo "<a class='projectButton' target='_blank' href='" . $button["button_link"] . "'>" . $button["button_text"] . "</a>";
+        echo "<a class='projectButton' target='_blank' href='" . $button["button_link"] . "'>" . $button["button_text"] . "</a><br>";
     }
 
     ?>
