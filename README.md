c404f1b4c86c3bc9964f517a992ad229ef8b3e9b
SIGIERLuc <lucsigier@gmail.com>
2019-04-18 20:26:07 +0200

    Reset

--- README.md
+++
@@ -1,1 +0,0 @@
-# maquette-cours-OCS
--- php/template-project-button.php
+++
@@ -1,3 +0,0 @@
-    <button data-toggle="collapse" data-target="#<?=$project_button['ID']?>" aria-controls="<?=$project_button['ID']?>">
-        <?=$project_button['project_name']?>
-    </button>
